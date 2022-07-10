e6644c8c81c0006e16503ef79fd3c38c04c68847
Siddiqui Muneeb <92376265+muneeb0321@users.noreply.github.com>
2022-07-10 15:03:42 +0530

    design-completed

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return      <div>
+
+    <div className="flex items-center justify-center my-6">
+
+        <p className='text-white text-xl font-extralight'> 1:15 pm | Sunday, 10 July 2022 
+        </p>
+    </div>
+
+    <div className="flex items-center justify-center my-6">
+
+        <p className='text-white text-3xl font-medium'> Mumbai,IN
+        </p>
+    </div>
+
+    
+
+
+    </div>
+  
+}
+
+export default TimeAndLocation
